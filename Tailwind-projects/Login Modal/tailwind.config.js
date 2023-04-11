a2a5161f53ae05f93a008c22e0b4b67ab717ae9f
Saurav Jaiswal <saurav14th@gmail.com>
2023-04-11 15:59:01 +0545

    Login Modal

---
+++ Tailwind-projects/Login Modal/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
